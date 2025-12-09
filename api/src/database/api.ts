5b46ca574465ec57e4a482135528fd068786b3bf
Sadousan <samueldouglas074@gmail.com>
2025-12-09 13:34:52 -0300

    eliminando redundância de api com import axios

--- api/src/database/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-  baseURL: "http://192.168.3.158:3333", // Seu IP de rede
-  timeout: 8000,
-});
-
-// Descubra pelo terminal com "ipconfig" (Windows feioso) ou "ip a" (Linux perfeito).
-// Depois testa no navegador do PC se http://SEU_IP:3333/ responde a rota "/" da API.
